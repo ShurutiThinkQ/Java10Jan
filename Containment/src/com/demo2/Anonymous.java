65ca6669d8b33a29b3a5351366a4973421a02056
hp <hp@DESKTOP-5ITQURM>
2023-02-02 11:38:15 +0530

    Ok

---
+++ Containment/src/com/demo2/Anonymous.java
@@ -0,0 +1,15 @@
+package com.demo2;
+
+public class Anonymous {
+
+	public static void main(String[] args) {
+
+     Employee e1= new Employee(101,"Bhushan",89000f,new Department(101,"HR","Delhi"));
+     Employee e2= new Employee(102,"Sneha",7800f, new Department(101,"HR","Mumbai"));
+     
+     System.out.println(e1);
+     System.out.println(e2);
+
+	}
+
+}
